b378a73c2eeaa9ba93ada011f94d41ef9ff17320
laugues <komix30@gmail.com>
2017-05-03 15:31:32 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
 window.BalanceLoaded();
+var BALANCE = {
+  var SERVER_URL = null;
+    function init(params){
+     SERVER_URL = params.SERVER_URL;
+    }
+
+function getAllSuppliers(callback){
+    _load(callback);
+}
+
+
+
+    function _load(callback) {
+        var defaultConfig = {};
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                  callback(JSON.parse(xhttp.responseText));
+                }
+            };
+            xhttp.open("GET", SERVER_URL+"/api/rest/suppliers", false);
+            xhttp.send();
+    }
+}
